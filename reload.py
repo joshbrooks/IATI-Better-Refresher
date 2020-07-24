bee12ea45b73735dc5a15db03892a7402b70b74c
Alex Lydiate <alexl@devinit.org>
2020-07-24 15:34:04 +0100

    Multiprocessing, but rough and ready. WIP

--- reload.py
+++ reload.py
@@ -1,3 +1,4 @@
+from multiprocessing import Process
 import os
 import argparse
 import requests
@@ -13,6 +14,7 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
 DATA_TABLENAME = "datasets"
+PARALLEL_PROCESSES = 10
 
 
 def requests_retry_session(
@@ -72,18 +74,30 @@ def main(args):
         )
 
     new_datasets = conn.execute(datasets.select().where(dataset_filter)).fetchall()
-    bar = progressbar.ProgressBar()
+
+    chunked_datasets= list(split(new_datasets, PARALLEL_PROCESSES))
+    
     print("Downloading {} datasets...".format(len(new_datasets)))
-    download_errors = 0
-    for dataset in bar(new_datasets):
-        file_destination = os.path.join(data_path, dataset["id"])
-        try:
-            download_file(dataset["url"], file_destination)
-            conn.execute(datasets.update().where(datasets.c.id == dataset["id"]).values(new=0, modified=0, stale=0, error=0))
-        except (requests.exceptions.ConnectionError, requests.exceptions.HTTPError) as e:
-            download_errors += 1
-            conn.execute(datasets.update().where(datasets.c.id == dataset["id"]).values(error=1))
-    print("Failed to download {} datasets.".format(download_errors))
+
+    processes = []
+    i = 0
+    
+    for chunk in chunked_datasets:
+        process = Process(target=download_chunk, args=(chunk, data_path, conn, datasets,))
+        process.start()
+        processes.append(process)
+
+    finished = False
+
+    while finished == False:
+        time.sleep(2)
+        finished = True
+        for process in processes:
+            process.join(timeout=0)
+            if proc.is_alive():
+                finished = False
+
+    print('Downloading processes all finished.')
 
     stale_datasets = conn.execute(datasets.select().where(datasets.c.stale == 1)).fetchall()
     print("Deleting {} stale datasets...".format(len(stale_datasets)))
@@ -97,9 +111,26 @@ def main(args):
 
     engine.dispose()
 
+def download_chunk(chunk, data_path, conn, datasets):
+    bar = progressbar.ProgressBar()
+    download_errors = 0
+    for dataset in bar(chunk):
+        file_destination = os.path.join(data_path, dataset["id"])
+        try:
+            download_file(dataset["url"], file_destination)
+            conn.execute(datasets.update().where(datasets.c.id == dataset["id"]).values(new=0, modified=0, stale=0, error=0))
+        except (requests.exceptions.ConnectionError, requests.exceptions.HTTPError) as e:
+            download_errors += 1
+            conn.execute(datasets.update().where(datasets.c.id == dataset["id"]).values(error=1))
+    print("Failed to download {} datasets.".format(download_errors))
+
+def split(lst, n):
+    k, m = divmod(len(lst), n)
+    return (lst[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Load IATI Registry packages.')
     parser.add_argument('-e', '--errors', dest='errors', action='store_true', default=False, help="Attempt to download previous errors")
     args = parser.parse_args()
     main(args)
+
