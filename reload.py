f5eaf6ac94e1e7905fe0821a218d8ed64ec0a09f
Alex Lydiate <alexl@devinit.org>
2020-07-27 09:27:17 +0100

    Multiprocessing working nicely, added vscode to gitignore

--- reload.py
+++ reload.py
@@ -1,5 +1,6 @@
 from multiprocessing import Process
 import os
+import time
 import argparse
 import requests
 import progressbar
@@ -9,13 +10,10 @@ from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
-
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-
 
 DATA_TABLENAME = "datasets"
 PARALLEL_PROCESSES = 10
-
 
 def requests_retry_session(
     retries=10,
@@ -36,7 +34,6 @@ def requests_retry_session(
     session.mount('https://', adapter)
     return session
 
-
 def download_file(download_url, file_destination):
     with requests_retry_session(retries=3).get(url=download_url, timeout=5, stream=True, verify=False) as r:
         r.raise_for_status()
@@ -46,6 +43,22 @@ def download_file(download_url, file_destination):
                     f.write(chunk)
         return file_destination
 
+def download_chunk(chunk, data_path, conn, datasets):
+    bar = progressbar.ProgressBar()
+    download_errors = 0
+    for dataset in bar(chunk):
+        file_destination = os.path.join(data_path, dataset["id"])
+        try:
+            download_file(dataset["url"], file_destination)
+            conn.execute(datasets.update().where(datasets.c.id == dataset["id"]).values(new=0, modified=0, stale=0, error=0))
+        except (requests.exceptions.ConnectionError, requests.exceptions.HTTPError) as e:
+            download_errors += 1
+            conn.execute(datasets.update().where(datasets.c.id == dataset["id"]).values(error=1))
+    print("Failed to download {} datasets.".format(download_errors))
+
+def split(lst, n):
+    k, m = divmod(len(lst), n)
+    return (lst[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
 
 def main(args):
     dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -80,9 +93,10 @@ def main(args):
     print("Downloading {} datasets...".format(len(new_datasets)))
 
     processes = []
-    i = 0
     
     for chunk in chunked_datasets:
+        if len(chunk) == 0:
+            continue
         process = Process(target=download_chunk, args=(chunk, data_path, conn, datasets,))
         process.start()
         processes.append(process)
@@ -94,13 +108,14 @@ def main(args):
         finished = True
         for process in processes:
             process.join(timeout=0)
-            if proc.is_alive():
+            if process.is_alive():
                 finished = False
 
     print('Downloading processes all finished.')
 
     stale_datasets = conn.execute(datasets.select().where(datasets.c.stale == 1)).fetchall()
     print("Deleting {} stale datasets...".format(len(stale_datasets)))
+
     for dataset in stale_datasets:
         file_destination = os.path.join(data_path, dataset["id"])
         try:
@@ -111,26 +126,10 @@ def main(args):
 
     engine.dispose()
 
-def download_chunk(chunk, data_path, conn, datasets):
-    bar = progressbar.ProgressBar()
-    download_errors = 0
-    for dataset in bar(chunk):
-        file_destination = os.path.join(data_path, dataset["id"])
-        try:
-            download_file(dataset["url"], file_destination)
-            conn.execute(datasets.update().where(datasets.c.id == dataset["id"]).values(new=0, modified=0, stale=0, error=0))
-        except (requests.exceptions.ConnectionError, requests.exceptions.HTTPError) as e:
-            download_errors += 1
-            conn.execute(datasets.update().where(datasets.c.id == dataset["id"]).values(error=1))
-    print("Failed to download {} datasets.".format(download_errors))
-
-def split(lst, n):
-    k, m = divmod(len(lst), n)
-    return (lst[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
+    print("Done.")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Load IATI Registry packages.')
     parser.add_argument('-e', '--errors', dest='errors', action='store_true', default=False, help="Attempt to download previous errors")
     args = parser.parse_args()
     main(args)
-
